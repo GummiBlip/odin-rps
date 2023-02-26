ca744a4e076ecf235d8a6778a84a417dd8ddcc00
GummiBlip <gummiblip@gmail.com>
2023-02-26 10:12:37 -0600

    Finish game loop

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function convertPlayerChoice(choice) {
     case 'scissors' || 'scissor':
       return scissorsData;
     default:
-      console.log("An unexpected issue has arisen. Check the code of the handlePlayerChoice function.");
+      console.log("An unexpected issue has arisen. Check the code of the convertPlayerChoice function.");
   }
 
 }
@@ -54,20 +54,44 @@ function printHelp() {
   console.log("This is a Rock Paper Scissors game written in JavaScript. Please enter 'Rock,' 'Paper,' or 'Scissors' to select your move.")
 }
 
-window.addEventListener('load', gameLoop())
+function getRoundResult(playerChoice, computerChoice) {
+  let result = null;
+  console.log(playerChoice.get(computerChoice.get("name")))
+  switch (playerChoice.get(computerChoice.get("name"))) {
+    case "beats":
+      return result = "win";
+    case "loses":
+      return result = "loss";
+    case "draws":
+      return result = "draw";
+    default:
+      console.log("An error has ocurred. Please check the getRoundResult function.")
+}
 
-function gameLoop(timesToPlay = 5) {
-  for (iterations = 0; iterations < timesToPlay; iterations++) {
-    let playerChoice = convertPlayerChoice(getPlayerChoice());
-    let computerChoice = getComputerChoice();
+}
 
-
+function announceRoundResult(playerRoundResult, playerChoice, computerChoice) {
+  switch (playerRoundResult) {
+    case "win":
+      console.log("You win! " + playerChoice.get("name") + " beats " + computerChoice.get("name"));
+      break;
+    case "loss":
+      console.log("You lose! " + playerChoice.get("name") + " loses to " + computerChoice.get("name"));
+      break;
+    case "draw":
+      console.log("It's a draw! " + playerChoice.get("name") + " draws with " + computerChoice.get("name"));
+      break;
   }
 }
 
-
-
-switch (computerChoice.get(playerChoice)) {
-  case "beats":
-    console.log("You lost!")
+function gameLoop(roundsToPlay = 5) {
+  for (rounds = 0; rounds < roundsToPlay; rounds++) {
+    let playerChoice = convertPlayerChoice(getPlayerChoice());
+    let computerChoice = getComputerChoice();
+    let roundResult = getRoundResult(playerChoice, computerChoice);
+    announceRoundResult(roundResult, playerChoice, computerChoice)
+    
+  }
 }
+
+window.addEventListener('load', gameLoop())
