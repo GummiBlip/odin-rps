ee7fb25f183f0d66c52d294716de065c92e397e5
GummiBlip <gummiblip@gmail.com>
2023-03-07 16:17:43 -0600

    Remove startGame function. Re-order functions

--- script.js
+++ script.js
@@ -17,77 +17,10 @@ let scissorsData = new Map([
   ["scissors", "draws"]
 ]);
 
-function getComputerChoice() {
-  let randomNumber = (Math.floor(Math.random() * 3));
-  switch (randomNumber) {
-    case 0:
-      return rockData;
-    case 1:
-      return paperData;
-    case 2:
-      return scissorsData;
-    default:
-      alert("An unexpected issue has arisen. Check the code of the getComputerChoice function.");
-  }
-}
-
-function getPlayerChoice() {
-  let input = prompt("Please enter your selection, or type help for help: ");
-  if (input === null) {
-    return input = "rock";
-  } else {
-    return input;
-  }
-}
-
-function stringToChoice(string) {
-  switch (string.toLowerCase()) {
-    case "rock":
-      return rockData;
-    case 'paper':
-      return paperData;
-    case 'scissors' || 'scissor':
-      return scissorsData;
-    default:
-      return null;
-  }
-
-}
-
-function printHelp() {
-  alert("This is a Rock Paper Scissors game written in JavaScript. Please enter 'Rock,' 'Paper,' or 'Scissors' to select your move.")
-}
-
-function capitalize(string) {
-  return string[0].toUpperCase() + string.slice(1);
-}
-
-function getRoundResult(playerChoice, computerChoice) {
-  switch (playerChoice.get(computerChoice.get("name"))) {
-    case "beats":
-      return "win";
-    case "loses":
-      return "lose";
-    case "draws":
-      return "draw";
-    default:
-      alert("An error has ocurred. Please check the getPlayerResult function.")
-      return null;
-}
-
-}
-
-function announceRoundResult(playerRoundResult, playerChoice, computerChoice) {
-  alert(`You ${playerRoundResult}! ${capitalize(playerChoice.get("name"))} ${playerRoundResult + "s"} against ${capitalize(computerChoice.get("name"))}.`);
-}
-
-function updateScore(playerRoundResult) {
-
-}
-
-function gameLoop(roundsToPlay = 5) {
-  let playerChoice
-  let computerChoice
+function gameLoop() {
+  let roundsToPlay = getDesiredRounds();
+  let playerChoice;
+  let computerChoice;
   for (rounds = 0; rounds < roundsToPlay; rounds++) {
     
     do {
@@ -116,8 +49,74 @@ function getDesiredRounds() {
     }
 }}
 
-function startGame() {
-  let desiredRounds = getDesiredRounds();
-  gameLoop(desiredRounds);
+function getPlayerChoice() {
+  let input = prompt("Please enter your selection, or type help for help: ");
+  if (input === null) {
+    return input = "rock";
+  } else {
+    return input;
+  }
 }
-window.addEventListener('load', startGame())
+
+function getComputerChoice() {
+  let randomNumber = (Math.floor(Math.random() * 3));
+  switch (randomNumber) {
+    case 0:
+      return rockData;
+    case 1:
+      return paperData;
+    case 2:
+      return scissorsData;
+    default:
+      alert("An unexpected issue has arisen. Check the code of the getComputerChoice function.");
+  }
+}
+
+function stringToChoice(string) {
+  switch (string.toLowerCase()) {
+    case "rock":
+      return rockData;
+    case 'paper':
+      return paperData;
+    case 'scissors' || 'scissor':
+      return scissorsData;
+    default:
+      return null;
+  }
+
+}
+
+function getRoundResult(playerChoice, computerChoice) {
+  switch (playerChoice.get(computerChoice.get("name"))) {
+    case "beats":
+      return "win";
+    case "loses":
+      return "lose";
+    case "draws":
+      return "draw";
+    default:
+      alert("An error has ocurred. Please check the getPlayerResult function.")
+      return null;
+}
+
+}
+
+function announceRoundResult(playerRoundResult, playerChoice, computerChoice) {
+  alert(`You ${playerRoundResult}! ${capitalize(playerChoice.get("name"))} ${playerRoundResult + "s"} against ${capitalize(computerChoice.get("name"))}.`);
+}
+
+function updateScore(playerRoundResult) {
+//To be written
+}
+
+function printHelp() {
+  alert("This is a Rock Paper Scissors game written in JavaScript. Please enter 'Rock,' 'Paper,' or 'Scissors' to select your move.")
+}
+
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+
+
+window.addEventListener("load", gameLoop())
