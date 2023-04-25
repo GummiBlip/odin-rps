9b7002a866b3712633fa48b69b409fd994186f4d
GummiBlip <gummiblip@gmail.com>
2023-04-25 17:47:07 -0500

    Refactor getMatchupResult to take strings

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 let rockData = new Map([
-  ["name", "rock"],
   ["rock", "draw"],
   ["paper", "lose"],
   ["scissors", "win"]
 ]);
 let paperData = new Map([
-  ["name", "paper"],
   ["rock", "win"],
   ["paper", "draw"],
   ["scissors", "lose"]
 ]);
 let scissorsData = new Map([
-  ["name", "scissors"],
   ["rock", "lose"],
   ["paper", "win"],
   ["scissors", "draw"]
 ]);
-
+let nameData = new Map([
+  ["rock", rockData],
+  ["scissors", scissorsData],
+  ["paper", paperData]
+])
 
 function initializeGame() {
-  if (roundsInput.value < 1) {
-    updateInfo("ERROR");
-  }
+  if (!validateRounds()) return -1;
+  setGamePage();
+
+  let startButton = document.querySelector("button.start");
+  let roundsSelection = document.querySelector(".roundsSelection");
+  let gameplayButtons = document.querySelector(".gameplay");
   startButton.style.display = "none";
   roundsSelection.style.display = "none";
   gameplayButtons.style.display = "flex";
+  
   for (const button of gameplayButtons.children) {
     button.addEventListener("click", playRound);
-    
   }
 }
 
 function playRound() {
-  console.log(this);
+  console.log(getRandomMove());
 }
 
-function updateScore(roundResult, score) {
+function validateRounds() {
+  let rounds = document.querySelector("#rounds").value;
+}
+
+function updateScore(roundResult) {
   switch (roundResult) {
     case "win":
       score[0]++;
@@ -63,10 +71,9 @@ function getRandomMove() {
   }
 }
 
-function getMatchupResult(move, target) {
-  return (move.get(target.get("name"))) 
+function getMatchupResult(moveName, targetName) {
+  return nameData.get(moveName).get(targetName);
 }
-
 
 function announceGameState(roundResult, playerMove, computerMove, score) {
   let announcementText = `You ${roundResult}! ${capitalize(playerMove.get("name"))} ${roundResult + "s"} against ${capitalize(computerMove.get("name"))}.`
@@ -74,11 +81,11 @@ function announceGameState(roundResult, playerMove, computerMove, score) {
     announcementText += ` This makes the score ${score[0]} for the player and ${score[1]} for the computer as of Round ${currentRound}.`;
   } else {
     if (score[0] > score[1]) {
-      announcementText += (`The player wins with a score of ${score[0]} to the computer's ${score[1]}!`);
+      announcementText += (` The player wins with a score of ${score[0]} to the computer's ${score[1]}!`);
     } else if (score[1] > score[0]) {
-      announcementText += (`The computer wins with a score of ${score[1]} to the player's ${score[0]}!`);
+      announcementText += (` The computer wins with a score of ${score[1]} to the player's ${score[0]}!`);
     } else {
-      announcementText += (`It's a draw! ${score[0]}-${score[1]}`);
+      announcementText += (` It's a draw! ${score[0]}-${score[1]}`);
       }}
   updateInfo(announcementText);
   }
@@ -92,8 +99,5 @@ function updateInfo(message) {
   informationParagraph.innerText = message;
 }
 
-let startButton = document.querySelector("button.start");
-let roundsSelection = document.querySelector(".roundsSelection");
-let roundsInput = document.querySelector("#rounds")
-let gameplayButtons = document.querySelector(".gameplay");
-startButton.addEventListener("click", initializeGame);
+let score = [0, 0];
+document.querySelector("button.start").addEventListener("click", initializeGame);
